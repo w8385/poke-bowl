9ff8cf59a4b6cc9f8a062e451bf9ad7ac26bb470
w8385 <w8385@naver.com>
2025-03-30 16:22:09 +0900

    feat: add Swagger documentation setup for Poké Bowl API

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Poké Bowl API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().then(() => console.log(`http://localhost:${process.env.PORT ?? 3000}/api`));
