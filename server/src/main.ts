a9fd0262d72764d8ff0399bfafb1e8a4fb294dab
w8385 <w8385@naver.com>
2025-03-30 17:01:32 +0900

    style: crlf

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Poké Bowl API')
-    .setVersion('1.0')
-    .build();
+  const config = new DocumentBuilder().setTitle('Poké Bowl API').setVersion('1.0').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap().then(() => console.log(`http://localhost:${process.env.PORT ?? 3000}/api`));
+bootstrap()
+  .then(() => console.log(`http://localhost:${process.env.PORT ?? 3000}/api`))
+  .catch((error) => console.error(error));
